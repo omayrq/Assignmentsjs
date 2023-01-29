bb826a6a5c06542e259c716e83b0f76d4a4b0bb0
Umair Ashraf <omayr.qureshi@gmail.com>
2023-01-30 00:58:18 +0500

    first assignment

---
+++ Assignment 01/app.js
@@ -0,0 +1,21 @@
+//    1. Write a script to greet your website visitor using JS alert box.
+
+   alert("Hello");
+
+//2. Write a script to display following message on your web page:
+
+    alert("Error! Please enter a valid password");
+
+// 3. Write a script to display following message on your web page: (Hint : Use line break)
+
+    alert("Wellcome to JS land.. \nHappy Coding!");
+
+// 4. Write a script to display following messages in sequence:
+
+   alert("Welcome to JS land...");
+    
+   alert("Happy Coding! \nPrevent this page from creating additional dialogs."); 
+
+// 5. Generate the following message through browser’s developer console:
+
+    alert("Hello... I can run JS through my web browser's console");
